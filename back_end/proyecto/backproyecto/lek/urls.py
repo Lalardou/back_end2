9881af0163985ec7e27b5324bb222cdaaff3c738
Jose Escobar <duocuc>
2022-06-08 01:27:36 -0400

    componentes

--- back_end/proyecto/backproyecto/lek/urls.py
+++ back_end/proyecto/backproyecto/lek/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import menu, componentes, contactos, nosotros,registro, login
+from .views import menu, componentes, contactos, nosotros,registro, login, carrito,audifono1,audifono2,audifono3, audifono4, audifono5
 
 urlpatterns = [
     path('',menu,name='menu'),
@@ -8,6 +8,10 @@ urlpatterns = [
     path('nosotros/',nosotros,name='nosotros'),
     path('registro/',registro,name='registro'),
     path('login/',login,name='login'),
-
-    
+    path('carrito/',carrito,name='carrito'),
+    path('audifono1/',audifono1, name='audifono1'),
+    path('audifono2/',audifono2, name='audifono2'),
+    path('audifono3/',audifono3, name='audifono3'),
+    path('audifono4/',audifono4, name='audifono4'),
+    path('audifono5/',audifono5, name='audifono5'),
 ]
